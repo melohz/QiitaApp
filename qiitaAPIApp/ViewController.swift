bda78bbc844e034ca94d35d6615af7ab78e8e08b
Tom <melooh42@gmail.com>
2019-07-22 21:08:38 +0900

    Initial Commit

---
+++ qiitaAPIApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qiitaAPIApp
+//
+//  Created by 板垣智也 on 2019/07/22.
+//  Copyright © 2019 板垣智也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ qiitaAPIAppTests/qiitaAPIAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  qiitaAPIAppTests.swift
+//  qiitaAPIAppTests
+//
+//  Created by 板垣智也 on 2019/07/22.
+//  Copyright © 2019 板垣智也. All rights reserved.
+//
+
+import XCTest
+@testable import qiitaAPIApp
+
+class qiitaAPIAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
